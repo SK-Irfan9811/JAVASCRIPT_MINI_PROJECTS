5824be6943e5f6c1a6dd3ce21fac715f2e3ab114
SK-Irfan9811 <108482864+SK-Irfan9811@users.noreply.github.com>
2022-08-17 00:07:44 +0530

    sounds for buttons
    playing audio when button is clicked

---
+++ demo7.js
@@ -0,0 +1,45 @@
+var a=document.getElementById("btn1");
+a.addEventListener('click',function(){
+    a.style.backgroundColor="red";
+    var audio =new Audio("s.wav");
+    audio.play();
+
+});
+var b=document.getElementById("btn2");
+b.addEventListener('click',function(){
+    b.style.backgroundColor="yellow";
+   
+    var audio =new Audio("s.wav");
+    audio.play();
+    
+});
+var c=document.getElementById("btn3");
+c.addEventListener('click',function(){
+    c.style.backgroundColor="blue";
+    var audio =new Audio("s.wav");
+    audio.play();
+    
+});
+var d=document.getElementById("btn4");
+d.addEventListener('click',function(){
+    d.style.backgroundColor="green";
+    var audio =new Audio("s.wav");
+    audio.play();
+    
+});
+var e=document.getElementById("btn5");
+e.addEventListener('click',function(){
+    e.style.backgroundColor="grey";
+    var audio =new Audio("s.wav");
+    audio.play();
+    
+});
+var f=document.getElementById("btn6");
+f.addEventListener('click',function(){
+    f.style.backgroundColor="pink";
+    var audio =new Audio("s.wav");
+    audio.play();
+    
+});
+
+
