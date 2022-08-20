2aeda9b7f24533ed27c17288128b5049b94a929a
SK-Irfan9811 <108482864+SK-Irfan9811@users.noreply.github.com>
2022-08-20 16:29:04 +0530

    Add user input
    adding user-defined input without spaces to list

--- demo9.js
+++ demo9.js
@@ -3,9 +3,16 @@ var btn=document.getElementById("add-item");
 var list=document.getElementById("todo-list");
 var count=4;
 btn.addEventListener('click',function(){
+    
     var x=document.createElement('li');
-    x.appendChild(document.createTextNode("ListItem"+count));
+    var y=document.getElementById('b');
+   
+    if(y.value.length==0 ){
+        alert("please enter items");
+    }else{
+    x.appendChild(document.createTextNode(y.value));
     count++;
+    y.value="";
     list.appendChild(x);
-    console.log(x);
+    }
 });
