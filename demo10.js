cb7d9b7baeaa6cf089e57527d519fcb767f2ac87
Irfan <isk22802@gmail.com>
2022-10-22 12:42:07 +0530

    okay

--- demo10.js
+++ demo10.js
@@ -3,7 +3,12 @@ var b3=document.getElementById("del-item");
 var t=document.getElementById("b");
 var list=document.getElementById("todo-list");
 var msg='';
+var count=document.getElementById("count");
+var api=document.getElementById("API");
+var pic=document.getElementById("imgg");
+var count=0;
 b.addEventListener('input',function(e){
+    
     msg=e.target.value;
 });
 b.addEventListener('keyup',function(e){
@@ -14,8 +19,10 @@ function del(e){
     var d=document.getElementById("item"+e);
     list.removeChild(d);
 }
+b1.addEventListener('click',postMethod)
 b1.addEventListener('click',addList)
 function addList(){
+    count++;
     if(msg.length!==0){
     var newDiv=document.createElement('div');
     var image=document.createElement('img');
@@ -33,19 +40,77 @@ function addList(){
     image.addEventListener('click',function(){
        del(image.className);
     })
+    msg='';
     }
     else{
         alert("Enter item first");
     }
+    
 }
-
 
 b3.addEventListener('click',function(){
     if(list.childElementCount>0){
     var first=list.firstElementChild;
     list.removeChild(first);
+    count--;
     }
     else{
         alert("no items to delete");
     }
 });
+function dynamicToDoList(id,url){
+    count++;
+    var newDiv=document.createElement('div');
+    newDiv.appendChild(document.createTextNode("id is "+id+"                 "))
+    newDiv.appendChild(document.createTextNode("URL is "+url));
+    newDiv.id="item"+list.childElementCount;
+    list.appendChild(newDiv);
+    
+}
+console.log(count);
+function postMethod(){
+    var http=new XMLHttpRequest();
+    http.onreadystatechange=function(){
+        if(this.readyState===4){
+            if(this.status===201){
+                var res=JSON.parse(this.responseText);
+                dynamicToDoList(res.id,msg);
+                console.log("item sent successfully");
+            }
+            else{
+                console.log("call failed");
+            }
+        }
+    }
+    http.open("POST","https://jsonplaceholder.typicode.com/photos",true);
+    var obj=JSON.stringify({
+        "albumId":msg,
+         "id":5001,
+         "url":"okok"
+    });
+    console.log("post Method");
+    http.send(obj);
+}
+function getApiData(){
+    var http=new XMLHttpRequest();
+    http.onreadystatechange=function(){
+        if(this.readyState===4){
+            if(this.status===200){
+                var sub=JSON.parse(this.responseText);
+                for(let i=0;i<sub.length;i++){
+                    dynamicToDoList(sub[i].id,sub[i].url);
+                    
+                } 
+            }
+            else{
+                console.log("call failed");
+            }
+        }
+    }
+    http.open("GET",'https://jsonplaceholder.typicode.com/photos',true);
+    http.send();
+    console.log(list.childElementCount);
+} 
+getApiData();
+
+
